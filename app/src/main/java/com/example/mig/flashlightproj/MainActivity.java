53b7853049920c807a8e6b9319197d9aaad9b79c
Mig <Chanachai_mig@hotmail.com>
2015-06-21 18:46:47 +0700

    Now can turn on/off flash light

--- app/src/main/java/com/example/mig/flashlightproj/MainActivity.java
+++ app/src/main/java/com/example/mig/flashlightproj/MainActivity.java
@@ -1,17 +1,90 @@
 package com.example.mig.flashlightproj;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Camera camera;
+    Button switchButton;
+    private boolean isFlashlightOn;
+    Camera.Parameters params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // flashlight on off Image
+        switchButton = (Button) findViewById(R.id.SwitchButton);
+
+        boolean isCameraFlash = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isCameraFlash) {
+            showNoCameraAlert();
+        } else {
+            camera = Camera.open();
+            params = camera.getParameters();
+        }
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashlightOn) {
+                    setFlashlightOff();
+                } else {
+                    setFlashlightOn();
+                }
+            }
+        });
+    }
+    private void showNoCameraAlert(){
+        new AlertDialog.Builder(this)
+                .setTitle("Error: No Camera Flash!")
+                .setMessage("Camera flashlight not available in this Android device!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish(); // close the Android app
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+        return;
+    }
+    private void setFlashlightOn() {
+        params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(params);
+        camera.startPreview();
+        isFlashlightOn = true;
+        //switchButton.setImageResource(R.drawable.light_on);
+    }
+
+    private void setFlashlightOff() {
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(params);
+        camera.stopPreview();
+        isFlashlightOn = false;
+        //flashlightSwitchImg.setImageResource(R.drawable.light_off);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 
     @Override
